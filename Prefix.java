920f5daa5273693c88bf79a35fa10c314a2d7658
conejoking <jzcastillo1987@gmail.com>
2023-10-26 01:24:14 -0500

    finished reductionLLP. Now ready for sweep down portion.

--- Prefix.java
+++ Prefix.java
@@ -15,11 +15,70 @@
 //             temp = A[k]
 //             A[k] = A[k] + A[k - stride/2]
 //             A[k - stride/2] = temp
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static java.lang.Math.log;
 
 public class Prefix
 {
-    public static void main(int[] arr)
-    {
-        System.out.println("Prefix.main()");
+        public static void main(int[] args) {
+            int[] data = args;
+            ForkJoinPool forkJoinPool = new ForkJoinPool();
+
+            int[] intermediateResults = new int[data.length];
+            AtomicInteger index = new AtomicInteger(0);
+
+            int sum = forkJoinPool.invoke(new SumTask(data, 0, data.length, intermediateResults, index));
+
+            System.out.println("Sum: " + sum);
+            System.out.println("Intermediate Results: " + java.util.Arrays.toString(intermediateResults));
+        }
+
+        static class SumTask extends RecursiveTask<Integer> {
+            private static final int THRESHOLD = 2;
+            private int[] data;
+            private int start;
+            private int end;
+            private int[] intermediateResults;
+            private AtomicInteger index;
+
+            SumTask(int[] data, int start, int end, int[] intermediateResults, AtomicInteger index) {
+                this.data = data;
+                this.start = start;
+                this.end = end;
+                this.intermediateResults = intermediateResults;
+                this.index = index;
+            }
+
+            @Override
+            protected Integer compute() {
+                int length = end - start;
+                if (length <= THRESHOLD) {
+                    int sum = 0;
+                    for (int i = start; i < end; i++) {
+                        sum += data[i];
+                    }
+                    //System.out.println(start/2+4);
+                    intermediateResults[start/2+ data.length/2] = sum;
+                    return sum;
+                } else {
+                    int mid = start + length / 2;
+                    SumTask leftTask = new SumTask(data, start, mid, intermediateResults, index);
+                    SumTask rightTask = new SumTask(data, mid, end, intermediateResults, index);
+
+                    leftTask.fork();
+                    int rightResult = rightTask.compute();
+                    int leftResult = leftTask.join();
+
+                    int sum = leftResult + rightResult;
+
+                    int diff = data.length/2-1;
+                    //System.out.println(treeH);
+                    intermediateResults[diff-index.getAndIncrement()] = sum;
+                    return sum;
+                }
+            }
+        }
     }
-}
+
